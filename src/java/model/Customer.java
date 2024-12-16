9c89d2509533b373186c57dd0647ce8807a39ae4
HuyHeoAn <163545666+HuyHeoAn@users.noreply.github.com>
2024-12-16 16:00:53 +0700

    xong

---
+++ src/java/model/Customer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author PC
+ */
+public class Customer {
+    private int id;
+    private String name;
+    private Date birthday;
+    private boolean gender;
+    private String email;
+    private String phone;
+    private String phone;
+    
+}
